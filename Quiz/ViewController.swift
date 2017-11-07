ce686ff1d2d5ef184588619ad96f18b61f8524a0
Ariel <ariel.scottdicker@shopify.com>
2017-11-06 21:25:17 -0500

    Animating constraints

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 	@IBOutlet var currentQuestionLabel: UILabel!
+	@IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
 	@IBOutlet var nextQuestionLabel: UILabel!
+	@IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
 	@IBOutlet var answerLabel: UILabel!
 	
 	let questions: [String] = [
@@ -30,6 +32,7 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		currentQuestionLabel.text = questions[currentQuestionIndex]
+		updateOffScreenLabel()
 	}
 	
 	override func viewWillAppear(_ animated: Bool) {
@@ -37,6 +40,11 @@ class ViewController: UIViewController {
 		
 		// Set the label's initial alpha
 		nextQuestionLabel.alpha = 0
+	}
+	
+	func updateOffScreenLabel() {
+		let screenWidth = view.frame.width
+		nextQuestionLabelCenterXConstraint.constant = -screenWidth
 	}
 	
 	@IBAction func showNextQuestion(_ sender: UIButton) {
@@ -58,17 +66,29 @@ class ViewController: UIViewController {
 	}
 	
 	func animateLabelTransitions() {
-		// Animate the alpha
+		// Force any outstanding layout changes to occur
+		view.layoutIfNeeded()
+		
+		// Animate the alpha and the center X constraints
+		let screenWidth = view.frame.width
+		nextQuestionLabelCenterXConstraint.constant = 0
+		currentQuestionLabelCenterXConstraint.constant += screenWidth
+		
 		UIView.animate(withDuration: 0.5,
 					   delay: 0,
 					   options: [],
 					   animations: {
 						self.currentQuestionLabel.alpha = 0
 						self.nextQuestionLabel.alpha = 1
+						self.view.layoutSubviews()
 		},
 					   completion: { _ in
 						swap(&self.currentQuestionLabel,
 							 &self.nextQuestionLabel)
+						swap(&self.currentQuestionLabelCenterXConstraint,
+							 &self.nextQuestionLabelCenterXConstraint)
+						
+						self.updateOffScreenLabel()
 		})		
 	}
 }
